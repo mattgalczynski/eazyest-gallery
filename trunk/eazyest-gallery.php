6b9367a3568e2920b51c64bdc12d40cf7e148578
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-01-21 09:54:50 +0000

    Fix issues in manually sorted images tables

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -7,14 +7,14 @@
  * Date: January 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-alpha-3-r28
+ * Version: 0.1.0-alpha-3-r29
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r28)  
+ * @version 0.1.0 (r29)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
