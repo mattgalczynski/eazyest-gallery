d9bc3af6f77f45bd340ba83e52badb56d14498b0
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-01-29 20:39:31 +0000

    options are now class properties

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: January 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-alpha-4-r48
+ * Version: 0.1.0-alpha-4-r49
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r48)  
+ * @version 0.1.0 (r49)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
