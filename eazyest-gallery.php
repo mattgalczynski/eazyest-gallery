ab64550591708f31dfd5c96913c455f76ceb482b
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-02-04 19:33:52 +0000

    Fix some attachment metadata issues

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: January 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-alpha-5-r67
+ * Version: 0.1.0-alpha-5-r68
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r67)  
+ * @version 0.1.0 (r68)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
