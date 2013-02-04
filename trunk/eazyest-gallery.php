979e8b94aa4148e5506643fa5a12ebfe5c8d3765
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-02-04 20:06:45 +0000

    Set Eazyest_Media_List_Table $args['screen'] to galleryfolder post_type to prevent undefined hook_suffix index

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: January 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-alpha-5-r68
+ * Version: 0.1.0-alpha-5-r69
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r68)  
+ * @version 0.1.0 (r69)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
