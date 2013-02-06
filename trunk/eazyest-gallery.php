3079a8765741fce2935618893c786eb212b8226f
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-02-06 13:31:52 +0000

    include path in i18n string

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: January 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-beta-1-r85
+ * Version: 0.1.0-beta-1-r88
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r85)  
+ * @version 0.1.0 (r88)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
