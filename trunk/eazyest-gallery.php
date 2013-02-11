7c5686bbfff078164491080275e7b6d3bfce1376
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-02-11 11:04:50 +0000

    0.1.0-beta-1

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: Frebruary 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-beta-1-r111
+ * Version: 0.1.0-beta-1
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r111)  
+ * @version 0.1.0 (r112)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
