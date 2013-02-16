aceb31ddfd2d5f78b3560e4317b6ea3357a17bb1
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-02-16 21:32:27 +0000

    Prevent Shadowbox JS to issue a missing source files message.

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: Frebruary 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-beta-2-r125
+ * Version: 0.1.0-beta-2-r126
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r125)  
+ * @version 0.1.0 (r126)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
