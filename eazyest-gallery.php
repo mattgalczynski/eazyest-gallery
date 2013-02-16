d2f2a77f5c535d139c9af4ec874ac926649661fd
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-02-16 22:20:51 +0000

    Make Icons per page and Thumbnails per page fill rows completely.

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: Frebruary 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-beta-2-r126
+ * Version: 0.1.0-beta-2-r127
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r126)  
+ * @version 0.1.0 (r127)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
