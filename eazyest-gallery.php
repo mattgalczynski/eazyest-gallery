3f5bbc760c9d996553145189d0412e02d8b869f2
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-02-18 09:02:52 +0000

    Fix messages in admin_head because current screen is empty in admin_init

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: February 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-beta-3-r136
+ * Version: 0.1.0-beta-3-r137
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r136)  
+ * @version 0.1.0 (r137)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
