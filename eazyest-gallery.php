2ef627f5182044773306ac2f996dc23e7f64ac41
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-02-20 08:35:18 +0000

    Apply 4:3 width:height aspect for camera slideshow div

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: February 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-beta-4-154
+ * Version: 0.1.0-beta-4-155
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r154)  
+ * @version 0.1.0 (r155)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
