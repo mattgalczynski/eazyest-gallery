940b2141a622c683d5aa2d14d8b52c1cebde7d86
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-02-26 13:19:03 +0000

    Add filter 'wp_generate_attachment_metadata' at priority 1 to set _cache filenames before other plugins want to use the files.

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: February 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-1-r177
+ * Version: 0.1.0-RC-1-r178
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r177)  
+ * @version 0.1.0 (r178)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
