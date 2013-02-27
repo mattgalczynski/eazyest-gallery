fc95a3a6e6cab6c43d7587873eb501b5c0ce8181
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-02-27 14:02:25 +0000

    Return correct value for $is_intermediate from image_downsize filter.

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -6,14 +6,14 @@
  * Date: February 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-1-r184
+ * Version: 0.1.0-RC-1-r185
  * License: GNU General Public License, version 3
  */
  
 /**
  * Eazyest Gallery is easy gallery management software for WordPress.
  * 
- * @version 0.1.0 (r184)  
+ * @version 0.1.0 (r185)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
