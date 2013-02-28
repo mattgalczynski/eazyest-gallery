d42d7e4562333e7cd97974b2ada70f3e5dd43b2a
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-02-28 08:09:03 +0000

    POT from 0.1.0-RC-1-187

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -8,11 +8,12 @@
  * Date: February 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-1-187
+ * Version: 0.1.0-RC-1-188
  * Text Domain: eazyest-gallery
+ * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r187)  
+ * @version 0.1.0 (r188)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
