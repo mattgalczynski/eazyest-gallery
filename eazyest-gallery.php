66e6996726bed893920b9a145256e4c98ed196db
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-03-08 08:54:39 +0000

    Implement Eazyest_Shortcodes::slideshow_shortcode() for [lg_slideshow]  shortcodes

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: March 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-4-r208
+ * Version: 0.1.0-RC-5-r209
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r208)  
+ * @version 0.1.0 (r209)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
