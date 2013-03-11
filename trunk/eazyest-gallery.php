0efa5b680daaff68da2a15f90107ab471d54e656
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-03-11 08:49:22 +0000

    Fix auto-index subfolders as root folder.

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: March 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-7-216
+ * Version: 0.1.0-RC-7-217
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r216)  
+ * @version 0.1.0 (r217)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
