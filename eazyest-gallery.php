eee8572dc3cb91dd1ec00a84fd2a410c1303901f
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-03-11 11:19:36 +0000

    Do not show Add subfolder link for draft folders.

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: March 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-7-217
+ * Version: 0.1.0-RC-7-218
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r217)  
+ * @version 0.1.0 (r218)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
