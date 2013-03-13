48ab8d6acbb28ff96e860e2c28422486b7ecd5ef
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-03-13 09:33:57 +0000

    Fix incorrect number of columns when more than one gallery display on page.

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: March 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-7-222
+ * Version: 0.1.0-RC-7-223
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r222)  
+ * @version 0.1.0 (r223)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
