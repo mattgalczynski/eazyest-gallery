83c79c918ce848c4c2b79c4f9bc2bf8e673c47eb
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-03-14 11:33:55 +0000

    Add pagination for subfolders and fix pagination for thumbnails.

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: March 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-7-225
+ * Version: 0.1.0-RC-7-226
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r225)  
+ * @version 0.1.0 (r226)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
