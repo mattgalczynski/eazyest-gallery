5a71f0e67545d151d81b26453fb92692132d602a
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-03-14 22:31:57 +0000

    Add AJAX navigation to paged subfolder archives.

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: March 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-7-231
+ * Version: 0.1.0-RC-7-232
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r231)  
+ * @version 0.1.0 (r232)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
