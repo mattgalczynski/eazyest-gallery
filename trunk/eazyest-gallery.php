09136076e87d3a1191f208f70d484b6f41693194
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-03-16 09:41:16 +0000

    Fix errors in Eazyest_FolderBase::featured_image()

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: March 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-8-239
+ * Version: 0.1.0-RC-8-240
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r239)  
+ * @version 0.1.0 (r240)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
