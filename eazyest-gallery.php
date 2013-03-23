c45b08cd5a9898557854616204f2ba62151f4c8a
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-03-23 22:41:28 +0000

    Do extra check before deleting folders.

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: March 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-9-253
+ * Version: 0.1.0-RC-9-254
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r253)  
+ * @version 0.1.0 (r254)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
