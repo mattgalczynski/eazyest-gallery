895b1cb753d59b15b0d60459290b8e12f25bf37f
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-04-09 10:50:22 +0000

    Eazyest Gallery 0.1.0 final

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -8,7 +8,7 @@
  * Date: April 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0-RC-14-315
+ * Version: 0.1.0
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
