09999ae6ecf138dab78fa33cb3028e51a1c0e58e
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-04-09 11:22:11 +0000

    trunk is now 0.2.0-alpha

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: April 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.1.0
+ * Version: 0.2.0-alpha
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.1.0 (r315)  
+ * @version 0.2.0 (r317)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
