37ed51e766238e2c02a4e2c39dc07172942dd2df
MarcelBrinkkemper@gmail.com <MarcelBrinkkemper@gmail.com@1de9bc90-2855-eb4d-2c42-c97ca6f24991>
2013-04-10 08:12:37 +0000

    Apply filter 'eazyest_gallery_subfolders_title' for subfolders H3 title.

--- trunk/eazyest-gallery.php
+++ trunk/eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: April 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.2.0-alpha-319
+ * Version: 0.2.0-alpha-320
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.2.0 (r319)  
+ * @version 0.2.0 (r320)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
