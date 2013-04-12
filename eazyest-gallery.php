cfbc3b0bb129b6cd3dd299382e6483b02cd5e3da
MarcelBrinkkemper <MarcelBrinkkemper@gmail.com>
2013-04-12 17:44:56 +0000

    Bug Fix: No captions display because of `style="display:none"`

--- eazyest-gallery.php
+++ eazyest-gallery.php
@@ -8,12 +8,12 @@
  * Date: April 2013
  * Author: Brimosoft
  * Author URI: http://brimosoft.nl
- * Version: 0.2.0-alpha-323
+ * Version: 0.2.0-alpha-324
  * Text Domain: eazyest-gallery
  * Domain Path: /languages/
  * License: GNU General Public License, version 3
  *
- * @version 0.2.0 (r323)  
+ * @version 0.2.0 (r324)  
  * @package Eazyest Gallery
  * @subpackage Main
  * @link http://brimosoft.nl/eazyest/gallery/
